959af8bdb531e0231b29dd1cf9cd70c54b474d73
Martin <martinkapfhammer@gmx.net>
2010-04-07 13:23:57 +0200

    - fix question logging

--- application/models/LogScore.php
+++ application/models/LogScore.php
@@ -69,10 +69,6 @@ class Model_LogScore extends Model_Score {
 			$this->resultDb->saveResult($questionId, $questionType, $result);
 			return '';
 		}
-		if ($existRecord['result'] === $result && $result === 'Y') {
-			// do nothing -> it is already in db
-			return '';
-		}
 		if (!$this->isReadyToUpdate($existRecord['date']) && $result === 'Y') {
 			// do nothing -> its not the time to set right 
 			return '';
