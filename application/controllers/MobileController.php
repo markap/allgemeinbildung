6d971d30d4e511c61f4ce74cac340a7f8e9a37b6
Martin <martinkapfhammer@gmx.net>
2010-09-25 15:35:43 +0200

    - mobile game ctr

--- application/controllers/MobileController.php
+++ application/controllers/MobileController.php
@@ -5,9 +5,10 @@ class MobileController extends Zend_Controller_Action
 
     public function init()
     {
-		if (!Model_MobileAuth::authentificate($this->_getParam('auth'))) {
-			exit();
+        if (!Model_MobileAuth::authentificate($this->_getParam('auth'))) {
+        	exit();
 		}
+		$this->_helper->layout->disableLayout();
     }
 
     public function indexAction()
@@ -17,13 +18,37 @@ class MobileController extends Zend_Controller_Action
 
     public function gamelistAction()
     {
-		$gameListDb = new Model_DbTable_GameList();
-		$json = $gameListDb->getGames(null, 'qtype = "def"');
-		echo Zend_Json::encode($json);	
+        $gameListDb 	= new Model_DbTable_GameList();
+		$json 			= $gameListDb->getGames(null, 'qtype = "def"');
+		echo Zend_Json::encode($json);
+    }
+
+    public function gameAction()
+    {
+		$gameId 		= $this->_getParam('gameid');
+		$gameListDb  	= new Model_DbTable_GameList();
+		$questionIds  	= $gameListDb->getQuestionIds($gameId);
+	
+		$questionDb 	= new Model_DbTable_Question();
+		$answerDb 		= new Model_DbTable_Answer();
+
+		$gameData = array();
+		foreach ($questionIds as $questionId) {
+			
+			$question   	= $questionDb->getQuestion($questionId, false);
+			$answer			= $answerDb->getAnswer($question['answerid']);
+			$questionData 	= array_merge($question, $answer);
+
+			$gameData[$questionId] = $questionData;
+		}	
+
+		echo Zend_Json::encode($gameData);
     }
 
 
 }
+
+
 
 
 
